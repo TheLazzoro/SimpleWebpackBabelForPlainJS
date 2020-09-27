175f68c525e7b439fae861b6f8a1fd8e4435a85e
Lars-m <lam@cphbusiness.dk>
2020-09-27 10:44:18 +0200

    Removed test credentials

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 
-# XXXX="Name or your frontend project, for example movie --> folder you created under /var/www"
-# DROPLET_URL="URL for your droplet"
-
-XXXX="movie"
-DROPLET_URL="165.227.128.191"
+XXXX="Name or your frontend project, for example movie --> folder you created under /var/www"
+DROPLET_URL="URL for your droplet"
 
 echo "##############################"
 echo "Building the frontend project"
